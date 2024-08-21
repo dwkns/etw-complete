5a1e2fa6834b94ccbf2eedf0e96d7c0905c7c38d
dwkns <dwkns@me.com>
2024-08-21 17:02:43 +0100

    Add console shortcode to eleventy

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,20 @@
-export default async function(eleventyConfig) {
+ import logToConsole from 'eleventy-plugin-console-plus'
 
+
+export default async  (eleventyConfig)=> {
+
+  // Copy Sanity Studio into build folder
   eleventyConfig.addPassthroughCopy({ "./sanity/dist": "./sanity" });
 
-    return {
-        dir: {
-          input: "src",
-          output: "dist",
-          includes: "_partials/",
-          layouts: "_partials/_layouts",
-          data: "_data",
-        }
-      }
-}
+  eleventyConfig.addPlugin(logToConsole);
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "_partials/",
+      layouts: "_partials/_layouts",
+      data: "_data"
+    }
+  }
+} 
